c571a32aa8143a475d082e99e47c85d041c6a096
Sivasankar53662 <sspengineeringsolutions@gmail.com>
2022-03-26 12:41:29 +0530

    Second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './common/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "home" , component : LoginComponent
+  },
+  {
+    path : "user" , loadChildren:()=>import("./modules/user/user.module").then(m=>m.UserModule)
+  },
+  {
+    path:"**" , redirectTo: '/home', pathMatch: 'full'
+  },
+  {
+    path: "", redirectTo: '/home', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './common/components/login/login.component';
+import { FooterComponent } from './common/components/footer/footer.component';
+import { HeaderComponent } from './common/components/header/header.component';
+import { PrimeNgModule } from './common/modules/prime-ng.module';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    PrimeNgModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
